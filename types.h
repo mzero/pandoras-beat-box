78b56e6f3c938627e82f53dc3fb0a776d13105af
Mark Lentczner <markl@glyphic.com>
2022-01-31 21:27:11 -0800

    add clamp & map_range template functions

--- types.h
+++ types.h
@@ -5,4 +5,29 @@ using micros_t = unsigned long;
 
 
 const bool plot_touch = false;
+const bool plot_axis = false;
+
+
+template < typename T >
+inline T clamp(const T& x, const T& lo, const T& hi) {
+  if (x < lo) return lo;
+  if (hi < x) return hi;
+  return x;
+}
+
+template < typename T >
+inline T map_range(const T& x,
+  const T& uLo, const T& uHi,
+  const T& vLo, const T& vHi)
+{
+  return (x - uLo) * ((vHi - vLo) / (uHi - uLo)) + vLo;
+}
+
+template < typename T >
+inline T map_range_clamped(const T& x,
+  const T& uLo, const T& uHi,
+  const T& vLo, const T& vHi)
+{
+  return clamp((x - uLo) * ((vHi - vLo) / (uHi - uLo)) + vLo, vLo, vHi);
+}
 
